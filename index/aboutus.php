21286535033ac1f807fecd1553ada67e7255e6fb
enjikal <franck.laurent07@orange.fr>
2023-04-13 14:18:15 +0200

    minorfixes

--- index/aboutus.php
+++ index/aboutus.php
@@ -14,7 +14,7 @@ include '../index/navbar.php';
 Nous sommes convaincus que notre alimentation devrait être simple, saine et savoureuse. Aussi,nous sommes fiers de partager ces recettes avec vous et nous espérons 
 que vous les apprécierez tout autant que nous.La cuisine est un art de vivre,des moments de partage, en famille ou entre amis. Nous avons donc rassemblé des recettes pour tous les goûts et toutes les occasions. De l'apéritif au dessert, en passant par les plats principaux, nous avons tout ce qu'il faut pour préparer un délicieux repas à la maison.
 Nous sommes ravis de partager notre passion pour la cuisine avec vous et nous espérons que vous apprécierez notre site et nos recettes.
- N'hésitez pas à nous contacter si vous avez des questions ou des commentaires. Bon appétit !
+ N'hésitez pas à nous contacter si vous avez des questions ou des commentaires.<br><h2>Bon appétit !</h2>
                 </div>
         
 </div>
