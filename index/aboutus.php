3ff15babdd9f0943083f8ee55fce60f99264403c
Franck le Frustré <franck.laurent07@orange.fr>
2023-05-03 20:45:42 +0200

    updatehome

--- index/aboutus.php
+++ index/aboutus.php
@@ -10,7 +10,7 @@ include '../index/navbar.php';
         <div class="welcome-title">
         <h1>Bienvenue sur notre site de recettes de cuisine "faites maison" !</h1>
         </div>
-                <div class="text-aboutus">Ici, vous trouverez une collection de délicieuses recettes, basée sur des ingrédients simples et de qualité.Ma maman m'a appris les secrets de la cuisine dès mon plus jeune âge et c'est grâce à elle que je suis devenu passionné de cuisine.Nous accordons une grande importance à l'utilisation d'ingrédients simples, sans additifs, sans colorants et sans conservateurs. 
+                <div class="text-aboutus">Ici, vous trouverez une collection de délicieuses recettes, basée sur des ingrédients simples et de qualité. Ma maman m'a appris les secrets de la cuisine dès mon plus jeune âge et c'est grâce à elle que je suis devenu passionné de cuisine. Nous accordons une grande importance à l'utilisation d'ingrédients simples, sans additifs, sans colorants et sans conservateurs. 
 Nous sommes convaincus que notre alimentation devrait être simple, saine et savoureuse. Aussi,nous sommes fiers de partager ces recettes avec vous et nous espérons 
 que vous les apprécierez tout autant que nous.La cuisine est un art de vivre,des moments de partage, en famille ou entre amis. Nous avons donc rassemblé des recettes pour tous les goûts et toutes les occasions. De l'apéritif au dessert, en passant par les plats principaux, nous avons tout ce qu'il faut pour préparer un délicieux repas à la maison.
 Nous sommes ravis de partager notre passion pour la cuisine avec vous et nous espérons que vous apprécierez notre site et nos recettes.
